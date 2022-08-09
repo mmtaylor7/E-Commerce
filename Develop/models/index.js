786d344cb1102e5f75531b8611a53f0fc373a9b2
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-09 10:28:46 -0400

    Products belongToMany Tags

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -16,6 +16,11 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id",
+});
+
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
