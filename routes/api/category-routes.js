0f4cb7bc72ce54b3c9a2ebc3866d608a48ec57f0
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-11 20:46:12 -0400

    create/update requests category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -21,10 +21,10 @@ router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const categoryId = await Category.findByPk(req.params.id, {
+    const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
-    res.status(200).json(categoryId);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -32,10 +32,31 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const cateogryData = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete("/:id", async (req, res) => {
