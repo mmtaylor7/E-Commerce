317ad15a3e5f869e108d9560849a8df1babea879
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-11 21:45:47 -0400

    subbed in "tag" for "product" in get req.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,10 +7,10 @@ router.get("/", async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const tagData = await Product.findAll({
-      include: [{ model: Category }, { model: Tag }],
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }],
     });
-    res.status(200).json(productData);
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
