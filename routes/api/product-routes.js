ead51f0788d45b1c7b5cbc72e9b585751933f706
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-11 21:42:46 -0400

    insomnia tested product routes check

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,7 +7,6 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 router.get("/", async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-
   try {
     const productData = await Product.findAll({
       include: [{ model: Category }, { model: Tag }],
