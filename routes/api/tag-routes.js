27781ab348522266b8051925b69c2d25bcacfb1e
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-11 21:49:18 -0400

    copied post req from cat. routes for template

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -31,6 +31,12 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", async (req, res) => {
   // create a new tag
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put("/:id", (req, res) => {
