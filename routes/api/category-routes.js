76011a08498e60cc9bad828aee11c5c7cc3f7386
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-11 22:39:46 -0400

    new post req for cat rout

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -30,14 +30,26 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
-  // create a new category
-  try {
-    const categoryData = await Category.create(req.body);
-    res.status(200).json(categoryData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+// router.post("/", async (req, res) => {
+//   // create a new category
+//   try {
+//     const categoryData = await Category.create({
+//       category_name: req.body.category_name,
+//     });
+//     res.status(200).json(categoryData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+router.post("/", (req, res) => {
+  Category.create(req.body)
+    .then((newCategory) => {
+      res.json(newCategory);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 router.put("/:id", async (req, res) => {
