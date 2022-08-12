2819a4a6f03f5a4fc719bcbe0eacc899944f76c2
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-11 21:48:18 -0400

    subbed out product in get req. for tag(by id)

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -20,10 +20,10 @@ router.get("/:id", async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const categoryData = await Category.findByPk(req.params.id, {
+    const tagData = await Tag.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
-    res.status(200).json(categoryData);
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
