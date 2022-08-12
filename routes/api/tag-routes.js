6de0a21dc1eb6176dceff6864c2ab960baa35dec
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-11 22:21:13 -0400

    async on put req tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -39,7 +39,7 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
   try {
     const tagData = await Tag.update(
