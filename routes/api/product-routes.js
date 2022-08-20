7847420b0d931d4ff351775f3262926e5530cb40
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-20 18:20:29 -0400

    product routes put

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -64,7 +64,7 @@ router.post("/", (req, res) => {
 });
 
 // update product
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
